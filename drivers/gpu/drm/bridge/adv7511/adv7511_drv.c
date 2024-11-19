998d2e9fb8e459a5a8776b118d2c4a6d4a14f859
Biju Das <biju.das.jz@bp.renesas.com>
2024-11-19 19:20:29 +0000

    drm: adv7511: Fix use-after-free in adv7533_attach_dsi()
    [ Upstream commit 81adbd3ff21c1182e06aa02c6be0bfd9ea02d8e8 ]
    
    The host_node pointer was assigned and freed in adv7533_parse_dt(), and
    later, adv7533_attach_dsi() uses the same. Fix this use-after-free issue
    by dropping of_node_put() in adv7533_parse_dt() and calling of_node_put()
    in error path of probe() and also in the remove().
    
    Fixes: 1e4d58cd7f88 ("drm/bridge: adv7533: Create a MIPI DSI device")
    Cc: stable@vger.kernel.org
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241119192040.152657-2-biju.das.jz@bp.renesas.com
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
+++ drivers/gpu/drm/bridge/adv7511/adv7511_drv.c
@@ -1305,8 +1305,10 @@ static int adv7511_probe(struct i2c_client *i2c, const struct i2c_device_id *id)
 		return ret;
 
 	ret = adv7511_init_regulators(adv7511);
-	if (ret)
-		return dev_err_probe(dev, ret, "failed to init regulators\n");
+	if (ret) {
+		dev_err_probe(dev, ret, "failed to init regulators\n");
+		goto err_of_node_put;
+	}
 
 	if (adv7511->addr_cec != 0)
 		cec_i2c_addr = adv7511->addr_cec << 1;
@@ -1434,6 +1436,8 @@ err_i2c_unregister_edid:
 	i2c_unregister_device(adv7511->i2c_edid);
 uninit_regulators:
 	adv7511_uninit_regulators(adv7511);
+err_of_node_put:
+	of_node_put(adv7511->host_node);
 #if IS_ENABLED(CONFIG_OF_DYNAMIC)
 	if (ret == -EPROBE_DEFER)
 		return ret;
@@ -1475,6 +1479,8 @@ uninit_regulators:
 static void adv7511_remove(struct i2c_client *i2c)
 {
 	struct adv7511 *adv7511 = i2c_get_clientdata(i2c);
+
+	of_node_put(adv7511->host_node);
 
 	adv7511_uninit_regulators(adv7511);
 
